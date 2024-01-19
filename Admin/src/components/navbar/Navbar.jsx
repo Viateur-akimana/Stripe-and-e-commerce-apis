186acd9ee5385de42c730cdc55b12814d97178c5
Viateur <akimanaviateur94@gmail.com>
2024-01-19 06:15:05 +0200

    Wokring on admin panel navbar component

---
+++ Admin/src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import navbar_logo from  "../../Assets/logo_big.png"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={navbar_logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
